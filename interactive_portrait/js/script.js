d44379b7a7f46435c5a38d79a4296191c0ceed3a
lc4490 <lc4490@nyu.edu>
2021-11-29 22:03:27 +0800

    interactive portraiture2

--- interactive_portrait/js/script.js
+++ interactive_portrait/js/script.js
@@ -1,5 +1,4 @@
 let cam;
-let r1, g1, b1;
 
 function setup() {
   createCanvas(640, 480);
@@ -7,59 +6,26 @@ function setup() {
   cam.hide();
 }
 
-function keyPressed(){
-  if(key == "r"){
-    r1=true;
-    g1=false;
-    b1=false;
-  }
-  else if(key == "g"){
-    g1=true;
-    r1=false;
-    b1=false;
-  }
-  else if(key == "b"){
-    b1=true;
-    g1=false;
-    r1=false;
-  }
-  else{
-    r1 = false;
-    g1=false;
-    b1=false;
-  }
-}
-
 function draw() {
+  background(0);
+
+  let gridSize = 15;
   cam.loadPixels();
-  for(let y = 0; y < height; y ++){
-    for(let x = 0; x < width; x++)
-      {
-        let index = (x + y *cam.width)*4;
-        let r = cam.pixels[index+0];
-        let g = cam.pixels[index+1];
-        let b = cam.pixels[index+2];
-        let a = cam.pixels[index+3];
+  for (let y = 0; y < cam.height; y+=gridSize) {
+    for (let x = 0; x < cam.width; x+=gridSize) {
+      let index = (x + y * cam.width) * 4;
 
-        let avg = (r+b+g)/3
+      let r = cam.pixels[index + 0];
+      let g = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      let avg = (r + g + b) / 3;
+      let dia = floor( map(avg, 0, 255, 0, 20) );
 
-        if(r1){
-          cam.pixels[index+0]= r;
-          cam.pixels[index+1]= 0;
-          cam.pixels[index+2]= 0;
-        }
-        else if(g1){
-          cam.pixels[index+0]= 0;
-          cam.pixels[index+1]= g;
-          cam.pixels[index+2]= 0;
-        }
-        else if(b1){
-          cam.pixels[index+0]= 0;
-          cam.pixels[index+1]= 0;
-          cam.pixels[index+2]= b;
-        }
-      }
+      fill(r,g,b);
+      noStroke();
+      ellipse(x, y, dia, dia);
+    }
   }
-  cam.updatePixels()
-  image(cam, 0,0);
+  cam.updatePixels();
+  // image(cam, 0, 0);
 }
